15b07143425ac68cd8efe7d2a65521f34e285fe7
Gene Edwards <eedwards@su.edu>
2020-04-05 00:37:54 -0400

    Initial files added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Test from "./components/Test"
+
+
+function App() {
+  return (
+    <div>
+     <Test></Test>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Test/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Test() {
+  return<div className="jumbotron jumbotron-fluid">
+  <div className="container">
+    <h1 className="display-4">Testing App</h1>
+    <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+  </div>
+</div>;
+}
+
+export default Test;
